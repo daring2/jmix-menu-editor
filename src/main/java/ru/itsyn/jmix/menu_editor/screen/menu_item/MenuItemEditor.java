d5d91d05091c24eacc9b6f7a4e14c17b7b70a434
daring <danila.daring@gmail.com>
2021-08-18 18:06:58 +0300

    #1 - MenuItemEntity - add caption property

--- src/main/java/ru/itsyn/jmix/menu_editor/screen/menu_item/MenuItemEditor.java
+++ src/main/java/ru/itsyn/jmix/menu_editor/screen/menu_item/MenuItemEditor.java
@@ -2,9 +2,12 @@ package ru.itsyn.jmix.menu_editor.screen.menu_item;
 
 import io.jmix.ui.component.CheckBox;
 import io.jmix.ui.component.ComboBox;
+import io.jmix.ui.component.TextField;
 import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.itsyn.jmix.menu_editor.entity.MenuItemEntity;
 import ru.itsyn.jmix.menu_editor.entity.MenuItemType;
+import ru.itsyn.jmix.menu_editor.util.MenuItemHelper;
 
 import javax.inject.Inject;
 
@@ -13,19 +16,32 @@ import javax.inject.Inject;
 @EditedEntityContainer("editDc")
 public class MenuItemEditor extends StandardEditor<MenuItemEntity> {
 
+    @Autowired
+    private MenuItemHelper menuItemHelper;
+    @Autowired
+    private TextField<String> idField;
+    @Autowired
+    private TextField<String> captionKeyField;
     @Inject
-    protected ComboBox<MenuItemType> itemTypeField;
+    private ComboBox<MenuItemType> itemTypeField;
     @Inject
-    protected CheckBox expandedField;
+    private CheckBox expandedField;
 
     @Subscribe
     public void onInit(InitEvent event) {
-        itemTypeField.addValueChangeListener(e -> updateFields());
+        initValueChangeListeners();
     }
 
-    protected void updateFields() {
-        var itemType= itemTypeField.getValue();
-        expandedField.setEditable(itemType == MenuItemType.MENU);
+    protected void initValueChangeListeners() {
+        idField.addValueChangeListener(e -> updateCaption());
+        captionKeyField.addValueChangeListener(e -> updateCaption());
+        itemTypeField.addValueChangeListener(e -> {
+            expandedField.setEditable(e.getValue() == MenuItemType.MENU);
+        });
+    }
+
+    protected void updateCaption() {
+        menuItemHelper.updateItemCaption(getEditedEntity());
     }
 
 }
