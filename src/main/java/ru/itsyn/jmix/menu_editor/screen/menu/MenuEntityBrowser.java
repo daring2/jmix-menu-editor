55724cdfda4c8530b95d5ae941b95426a991e1b7
daring <danila.daring@gmail.com>
2021-08-26 12:37:25 +0300

    #1 - menu-entity-browser.xml - add copy action

--- src/main/java/ru/itsyn/jmix/menu_editor/screen/menu/MenuEntityBrowser.java
+++ src/main/java/ru/itsyn/jmix/menu_editor/screen/menu/MenuEntityBrowser.java
@@ -1,7 +1,13 @@
 package ru.itsyn.jmix.menu_editor.screen.menu;
 
+import io.jmix.core.MetadataTools;
+import io.jmix.core.UuidProvider;
+import io.jmix.ui.ScreenBuilders;
+import io.jmix.ui.action.Action.ActionPerformedEvent;
+import io.jmix.ui.component.Table;
 import io.jmix.ui.navigation.Route;
 import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.itsyn.jmix.menu_editor.entity.MenuEntity;
 
 @Route("MenuEntity")
@@ -9,4 +15,25 @@ import ru.itsyn.jmix.menu_editor.entity.MenuEntity;
 @UiDescriptor("menu-entity-browser.xml")
 @LookupComponent("table")
 public class MenuEntityBrowser extends StandardLookup<MenuEntity> {
+
+    @Autowired
+    MetadataTools metadataTools;
+    @Autowired
+    ScreenBuilders screenBuilders;
+    @Autowired
+    Table<MenuEntity> table;
+
+    @Subscribe("table.copy")
+    public void onMenuCopy(ActionPerformedEvent event) {
+        var entity = table.getSingleSelected();
+        if (entity == null)
+            return;
+        var ce = metadataTools.copy(entity);
+        ce.setId(UuidProvider.createUuid());
+        ce.setName(entity.getName() + " - Copy");
+        screenBuilders.editor(table)
+                .newEntity(ce)
+                .show();
+    }
+
 }
