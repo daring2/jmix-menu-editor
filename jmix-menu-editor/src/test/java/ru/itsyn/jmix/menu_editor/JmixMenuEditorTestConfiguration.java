dbba93948c62e6ad07ad228fec60f912653f7505
daring <danila.daring@gmail.com>
2022-03-22 18:52:57 +0300

    #1 - add unit tests

--- jmix-menu-editor/src/test/java/ru/itsyn/jmix/menu_editor/JmixMenuEditorTestConfiguration.java
+++ jmix-menu-editor/src/test/java/ru/itsyn/jmix/menu_editor/JmixMenuEditorTestConfiguration.java
@@ -1,9 +1,12 @@
 package ru.itsyn.jmix.menu_editor;
 
 import io.jmix.core.annotation.JmixModule;
+import io.jmix.core.security.InMemoryUserRepository;
+import io.jmix.core.security.UserRepository;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -11,17 +14,23 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import javax.sql.DataSource;
 
 @SpringBootConfiguration
+@ComponentScan
 @EnableAutoConfiguration
-@JmixModule(dependsOn = JmixMenuEditorConfiguration.class)
+@JmixModule(id = "jmix-menu-editor-test", dependsOn = JmixMenuEditorConfiguration.class)
 public class JmixMenuEditorTestConfiguration {
 
     @Bean
     @Primary
-    DataSource dataSource() {
+    public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
                 .generateUniqueName(true)
                 .setType(EmbeddedDatabaseType.HSQL)
                 .build();
+    }
+
+    @Bean
+    public UserRepository userRepository() {
+        return new InMemoryUserRepository();
     }
 
 }
---
+++ jmix-menu-editor/src/test/java/ru/itsyn/jmix/menu_editor/screen/menu_config/MenuConfigLoaderTest.java
@@ -0,0 +1,27 @@
+package ru.itsyn.jmix.menu_editor.screen.menu_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MenuConfigLoaderTest {
+
+    @Autowired
+    MenuConfigLoader configLoader;
+
+    @Test
+    public void testLoadDefaultConfig() {
+        var items = configLoader.loadDefaultConfig();
+        assertEquals(1, items.size());
+        var rootItem = items.get(0);
+        assertEquals("administration", rootItem.getId());
+        assertEquals(2, rootItem.getChildren().size());
+        var item1 = rootItem.getChildren().get(1);
+        assertEquals("menu_MenuEntity.browse", item1.getId());
+        assertEquals(item1.getId(), item1.getScreen());
+    }
+
+}
