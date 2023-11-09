0e9b4bdfa2550fb2e4aa2bd70efb8a03c66ed005
daring <danila.daring@gmail.com>
2023-11-09 21:05:44 +0300

    #6 - migrate to jmix-2.x

--- jmix-menu-editor/src/main/java/ru/itsyn/jmix/menu_editor/JmixMenuEditorConfiguration.java
+++ jmix-menu-editor/src/main/java/ru/itsyn/jmix/menu_editor/JmixMenuEditorConfiguration.java
@@ -3,8 +3,9 @@ package ru.itsyn.jmix.menu_editor;
 import io.jmix.core.annotation.JmixModule;
 import io.jmix.core.impl.scanning.AnnotationScanMetadataReaderFactory;
 import io.jmix.eclipselink.EclipselinkConfiguration;
-import io.jmix.ui.UiConfiguration;
-import io.jmix.ui.sys.UiControllersConfiguration;
+import io.jmix.flowui.FlowuiConfiguration;
+import io.jmix.flowui.sys.ActionsConfiguration;
+import io.jmix.flowui.sys.ViewControllersConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -14,21 +15,33 @@ import org.springframework.context.annotation.PropertySource;
 
 import java.util.Collections;
 
+import static java.util.Collections.singletonList;
+
 @Configuration
 @ComponentScan
 @ConfigurationPropertiesScan
-@JmixModule(dependsOn = {EclipselinkConfiguration.class, UiConfiguration.class})
+@JmixModule(dependsOn = {EclipselinkConfiguration.class, FlowuiConfiguration.class})
 @PropertySource(name = "ru.itsyn.jmix.menu_editor", value = "classpath:/ru/itsyn/jmix/menu_editor/module.properties")
 public class JmixMenuEditorConfiguration {
 
     @Bean("menu_JmixMenuEditorUiControllers")
-    public UiControllersConfiguration screens(
+    public ViewControllersConfiguration screens(
             ApplicationContext applicationContext,
             AnnotationScanMetadataReaderFactory metadataReaderFactory
     ) {
-        var uiControllers = new UiControllersConfiguration(applicationContext, metadataReaderFactory);
-        uiControllers.setBasePackages(Collections.singletonList("ru.itsyn.jmix.menu_editor"));
-        return uiControllers;
+        var viewControllers = new ViewControllersConfiguration(applicationContext, metadataReaderFactory);
+        viewControllers.setBasePackages(singletonList("ru.itsyn.jmix.menu_editor"));
+        return viewControllers;
+    }
+
+    @Bean("menu_MenuActions")
+    public ActionsConfiguration actions(
+            ApplicationContext applicationContext,
+            AnnotationScanMetadataReaderFactory metadataReaderFactory
+    ) {
+        var actions = new ActionsConfiguration(applicationContext, metadataReaderFactory);
+        actions.setBasePackages(Collections.singletonList("ru.itsyn.jmix.menu_editor"));
+        return actions;
     }
 
 }
--- jmix-menu-editor/src/test/java/ru/itsyn/jmix/menu_editor/JmixMenuEditorTestConfiguration.java
+++ jmix-menu-editor/src/test/java/ru/itsyn/jmix/menu_editor/JmixMenuEditorTestConfiguration.java
@@ -1,36 +1,31 @@
 package ru.itsyn.jmix.menu_editor;
 
 import io.jmix.core.annotation.JmixModule;
-import io.jmix.core.security.InMemoryUserRepository;
-import io.jmix.core.security.UserRepository;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
 @SpringBootConfiguration
-@ComponentScan
 @EnableAutoConfiguration
-@JmixModule(id = "jmix-menu-editor-test", dependsOn = JmixMenuEditorConfiguration.class)
+@Import(JmixMenuEditorConfiguration.class)
+@PropertySource("classpath:/ru/itsyn/jmix/menu_editor/test-app.properties")
+@JmixModule(id = "ru.itsyn.jmix.menu_editor.test", dependsOn = JmixMenuEditorConfiguration.class)
 public class JmixMenuEditorTestConfiguration {
 
     @Bean
     @Primary
-    public DataSource dataSource() {
+    DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
                 .generateUniqueName(true)
                 .setType(EmbeddedDatabaseType.HSQL)
                 .build();
-    }
-
-    @Bean
-    public UserRepository userRepository() {
-        return new InMemoryUserRepository();
     }
 
 }
